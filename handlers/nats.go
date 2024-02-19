36e223b728e39e7b6842394cc234c831d4b1252e
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-02-19 17:20:04 +0800

    nats

--- handlers/nats.go
+++ handlers/nats.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"time"
 
@@ -15,14 +17,38 @@ type (
 )
 
 func (filter *NATSFilter) Handle(r *http.Request) (*http.Response, error) {
+	req, err := CloneRequest(r)
+	if err != nil {
+		return nil, err
+	}
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
 	conn, err := di.ResolveWithName[nats.Conn](filter.Address.Scheme, nil)
 	if err != nil {
 		return nil, err
 	}
-	res, err := conn.Request(filter.Address.Opaque, nil, time.Minute)
+	msg := nats.Msg{}
+	msg.Subject = filter.Address.Opaque
+	msg.Data = data
+	msg.Header = nats.Header{}
+	for key, values := range req.Header {
+		for _, value := range values {
+			msg.Header.Add(key, value)
+		}
+	}
+	res, err := conn.RequestMsg(&msg, time.Minute)
 	if err != nil {
 		return nil, err
 	}
-	_ = res
-	return nil, nil
+	response := http.Response{}
+	response.Header = http.Header{}
+	response.Body = io.NopCloser(bytes.NewBuffer(res.Data))
+	for key, values := range res.Header {
+		for _, value := range values {
+			response.Header.Add(key, value)
+		}
+	}
+	return &response, nil
 }
