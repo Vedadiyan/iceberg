fd80ca9468b090bcc3e87295b6e2df843895767c
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-02-19 13:46:00 +0800

    moveto

--- handlers/http.go
+++ handlers/http.go
@@ -24,5 +24,19 @@ func (filter *HttpFilter) Is(level Level) bool {
 }
 
 func (filter *HttpFilter) MoveTo(res *http.Response, req *http.Request) error {
+	if filter.Is(PARALLEL) {
+		return nil
+	}
+	for _, header := range filter.ExchangeHeaders {
+		values := res.Header[header]
+		if len(values) > 0 {
+			for _, value := range values {
+				req.Header.Add(header, value)
+			}
+		}
+	}
+	if filter.ExchangeBody {
+		req.Body = res.Body
+	}
 	return nil
 }
