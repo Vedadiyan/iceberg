9cbc95b1d461cd7c02167c6e93facaf09a0b81c1
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-02-21 15:41:04 +0800

    grpc

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/websocket v1.5.1
 	github.com/nats-io/nats.go v1.33.1
 	github.com/vedadiyan/goal v0.2.11
+	google.golang.org/grpc v1.61.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -13,7 +14,7 @@ require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
@@ -24,10 +25,11 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto v0.0.0-20210701191553-46259e63a0a9 // indirect
-	google.golang.org/grpc v1.45.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/genproto v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- handlers/grpc.go
+++ handlers/grpc.go
@@ -1,7 +1,77 @@
 package handlers
 
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+
+	"github.com/vedadiyan/goal/pkg/di"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
 type (
 	GRPCFilter struct {
 		FilterBase
+		Url     string
+		Subject string
 	}
+	GRPCCodec struct{}
 )
+
+var (
+	_codec = GRPCCodec{}
+)
+
+// Marshal implements encoding.Codec.
+func (GRPCCodec) Marshal(v any) ([]byte, error) {
+	panic("unimplemented")
+}
+
+// Name implements encoding.Codec.
+func (GRPCCodec) Name() string {
+	panic("unimplemented")
+}
+
+// Unmarshal implements encoding.Codec.
+func (GRPCCodec) Unmarshal(data []byte, v any) error {
+	panic("unimplemented")
+}
+
+func (filter *GRPCFilter) Handle(r *http.Request) (*http.Response, error) {
+	req, err := CloneRequest(r, WithUrl(filter.Address))
+	if err != nil {
+		return nil, err
+	}
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	client, err := di.ResolveWithName[grpc.ClientConn](filter.Url, nil)
+	if err != nil {
+		return nil, err
+	}
+	reply := make([]byte, 0)
+	md := metadata.MD{}
+	for key, values := range req.Header {
+		md.Set(key, values...)
+	}
+	ctx := context.TODO()
+	err = client.Invoke(ctx, filter.Subject, data, &reply, grpc.ForceCodec(_codec), grpc.Header(&md))
+	if err != nil {
+		return nil, err
+	}
+	response := http.Response{}
+	response.Header = http.Header{}
+	response.Body = io.NopCloser(bytes.NewBuffer(reply))
+	headers, ok := metadata.FromOutgoingContext(ctx)
+	if ok {
+		for key, values := range headers {
+			for _, value := range values {
+				response.Header.Add(key, value)
+			}
+		}
+	}
+	return &response, nil
+}
