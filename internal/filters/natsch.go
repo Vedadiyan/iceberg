5d03537ce03e4b0596c8c76bd007590cfa207ec7
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-05-30 18:12:44 +0800

    parser

--- internal/filters/natsch.go
+++ internal/filters/natsch.go
@@ -57,22 +57,13 @@ func (filter *NATSCHFilter) HandleSync(r *http.Request) (*http.Response, error) 
 	natschMsg := natsch.WrapMessage(msg)
 	natschMsg.Deadline = time.Now().Add(time.Second * time.Duration(filter.Deadline)).UnixMicro()
 	msg.Reply = fmt.Sprintf("ICEBERGREPLY.%s", filter.Subject)
+	msg.Header.Add("reply", conn.NewRespInbox())
 	var wg sync.WaitGroup
 	var res *nats.Msg
 	if len(filter.Filters) > 0 {
 		wg.Add(1)
 		unsubscriber, err := conn.Subscribe(msg.Reply, func(msg *nats.Msg) {
 			res = msg
-			req, err := RequestFrom(MsgToResponse(msg))
-			if err != nil {
-				logger.Error(err, "")
-				return
-			}
-			err = HandleFilter(req, filter.Filters, INHERIT)
-			if err != nil {
-				logger.Error(err, "")
-				return
-			}
 		})
 		if err != nil {
 			logger.Error(err, "")
@@ -103,25 +94,6 @@ func (filter *NATSCHFilter) HandleAsync(r *http.Request) {
 	natschMsg := natsch.WrapMessage(msg)
 	natschMsg.Deadline = time.Now().Add(time.Second * time.Duration(filter.Deadline)).UnixMicro()
 	msg.Reply = fmt.Sprintf("ICEBERGREPLY.%s", filter.Subject)
-	if len(filter.Filters) > 0 {
-		unsubscriber, err := conn.Subscribe(msg.Reply, func(msg *nats.Msg) {
-			req, err := RequestFrom(MsgToResponse(msg))
-			if err != nil {
-				logger.Error(err, "")
-				return
-			}
-			err = HandleFilter(req, filter.Filters, INHERIT)
-			if err != nil {
-				logger.Error(err, "")
-				return
-			}
-		})
-		if err != nil {
-			logger.Error(err, "")
-			return
-		}
-		unsubscriber.AutoUnsubscribe(1)
-	}
 	err = conn.PublishMsgSch(natschMsg)
 	if err != nil {
 		logger.Error(err, "")
@@ -139,6 +111,16 @@ func (filter *NATSCHFilter) DurableSubscription() {
 		reply := msg.Header.Get("reply")
 		msg.Reply = reply
 		err := conn.Conn.PublishMsg(msg.Msg)
+		if err != nil {
+			logger.Error(err, "")
+			return
+		}
+		req, err := RequestFrom(MsgToResponse(msg.Msg))
+		if err != nil {
+			logger.Error(err, "")
+			return
+		}
+		err = HandleFilter(req, filter.Filters, INHERIT)
 		if err != nil {
 			logger.Error(err, "")
 			return
