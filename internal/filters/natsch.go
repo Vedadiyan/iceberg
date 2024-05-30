840eaf2e46a8b4405056977c9c395571a7e38dee
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-05-30 17:54:11 +0800

    parser

--- internal/filters/natsch.go
+++ internal/filters/natsch.go
@@ -57,10 +57,12 @@ func (filter *NATSCHFilter) HandleSync(r *http.Request) (*http.Response, error) 
 	natschMsg.Deadline = time.Now().Add(time.Second * time.Duration(filter.Deadline)).UnixMicro()
 	msg.Reply = fmt.Sprintf("ICEBERGREPLY.%s", filter.Subject)
 	var wg sync.WaitGroup
+	var res *natsch.Msg
 	if len(filter.Filters) > 0 {
 		wg.Add(1)
 		_, err = conn.QueueSubscribeSch(fmt.Sprintf("ICEBERGREPLY.%s", filter.Subject), "balanced", func(msg *natsch.Msg) {
 			defer wg.Done()
+			res = msg
 			req, err := RequestFrom(MsgToResponse(msg.Msg))
 			if err != nil {
 				logger.Error(err, "")
@@ -83,7 +85,7 @@ func (filter *NATSCHFilter) HandleSync(r *http.Request) (*http.Response, error) 
 		return nil, err
 	}
 	wg.Wait()
-	return nil, nil
+	return MsgToResponse(res.Msg)
 }
 
 func (filter *NATSCHFilter) HandleAsync(r *http.Request) {
