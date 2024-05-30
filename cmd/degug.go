c3eef3df38d1b16b1b47111e721b0c1c5ef8a006
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-05-30 15:14:49 +0800

    advanced filters

--- cmd/degug.go
+++ cmd/degug.go
@@ -5,6 +5,12 @@ package main
 import (
 	"log"
 	"os"
+
+	"github.com/vedadiyan/iceberg/internal/logger"
+)
+
+type (
+	DebugLogger struct{}
 )
 
 func init() {
@@ -13,4 +19,23 @@ func init() {
 		log.Fatalln(err)
 	}
 	os.Setenv("ICEBERG_CONFIG", string(config))
+	logger.AddLogger(&DebugLogger{})
+}
+
+func (*DebugLogger) Info(message string, params ...any) {
+	payload := []any{"INFO", message}
+	payload = append(payload, params...)
+	log.Println(payload...)
+}
+
+func (*DebugLogger) Warning(message string, params ...any) {
+	payload := []any{"WARNING", message}
+	payload = append(payload, params...)
+	log.Println(payload...)
+}
+
+func (*DebugLogger) Error(err error, message string, params ...any) {
+	payload := []any{"ERROR", err, message}
+	payload = append(payload, params...)
+	log.Println(payload...)
 }
