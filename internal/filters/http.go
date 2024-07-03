f658f762e03cc5a5b3f384292f9f21092ea8c7bc
Pouya Vedadiyan <vedadiyan@gmail.com>
2024-07-03 11:18:56 +0800

    errors

--- internal/filters/http.go
+++ internal/filters/http.go
@@ -20,7 +20,7 @@ func NewHttpFilter(f *Filter) *HttpFilter {
 	return httpFilter
 }
 
-func (f *HttpFilter) Call(ctx context.Context, c netio.Cloner) (netio.Next, *http.Response, *netio.Error) {
+func (f *HttpFilter) Call(ctx context.Context, c netio.Cloner) (netio.Next, *http.Response, netio.Error) {
 	r, err := c(netio.WithUrl(f.Address), netio.WithContext(ctx))
 	if err != nil {
 		return netio.TERM, nil, netio.NewError(err.Error(), http.StatusInternalServerError)
